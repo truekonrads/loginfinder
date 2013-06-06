f3969696e4bd48cf76d0e8b598681918f15a4163
Konrads Smelkovs <konrads.smelkovs@kpmg.co.uk>
2013-06-06 23:25:16 +0100

    First working version

--- findloginforms.rb
+++ findloginforms.rb
@@ -13,25 +13,28 @@ def main
   opts = Trollop::options do
 
     version "Login form identifer v0.31337"
-    banner <<-EOS Example usage:
+    banner <<-EOS
+Example usage:
       #{File.basename($0)} -u http://www.hack.com/ -c forms.csv
 EOS
     opt :url, "URL to crawl", :type => :string, :required => true
     # opt :csv,		"CSV file for output", :required => false, :type => :string
     opt :verbose,	"Verbose"
+    opt :threads, "Number of threads", :type => :int, :default => 4
 
   end
 
 
 
-  $VERBOSE=opts[:verbose]
-  LOGIN_WORDS="login auth".split(" ")
+  $unsilent=opts[:verbose]
   ff=FormFinder.new
-  Anemone.crawl(opts[:url]) do |anemone|
+  Anemone.crawl(opts[:url], :threads=>opts[:threads]) do |anemone|
+    anemone.skip_links_like /\.(doc.?|pdf|jpg|jpeg|xls.?|png|tif.?|)/
     anemone.on_every_page do |page|
-      puts "Crawling #{page.url}" if $VERBOSE
+      puts "Crawling #{page.url}" if $unsilent
       (ff.findAllForms page.doc).each {|f| 
-        puts "Form with action '#{f['action']}' found at #{page.url}"}
+        puts "Form with action '#{f['action']}' found at #{page.url}"
+        } if page.doc
     end
   end
 end
